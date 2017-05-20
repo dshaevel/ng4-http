4b78c7e32eca55c0d20d8474f5d69c07283fc9f4
David Shaevel <david@davidshaevel.com>
2017-05-19 19:20:12 -0500

    CHECKPOINT 2: adjusting request headers

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Headers, Http } from '@angular/http';
 
 @Injectable()
 export class ServerService {
   constructor(private http: Http) {}
 
   storeServers(servers: any[]) {
-    return this.http.post('https://udemy-ng-http-459a5.firebaseio.com/data.json', servers);
+    const headers = new Headers({'Content-Type': 'application/json'});
+    return this.http.post(
+      'https://udemy-ng-http-459a5.firebaseio.com/data.json',
+      servers,
+      {headers: headers}
+    );
   }
 }
